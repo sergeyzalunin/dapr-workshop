3b622d2b676fb6a0ebef5dfd4bd208489aded008
Sergey Zalunin <Sergey.Zalunin@bki-okb.ru>
2021-04-22 22:34:28 +0300

    a4-3

--- src/TrafficControlService/Repositories/DaprVehicleStateRepository.cs
+++ src/TrafficControlService/Repositories/DaprVehicleStateRepository.cs
@@ -2,39 +2,30 @@
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
+using Dapr.Client;
 using TrafficControlService.Models;
 
 namespace TrafficControlService.Repositories
 {
     public class DaprVehicleStateRepository : IVehicleStateRepository
     {
-        private readonly HttpClient _httpClient;
+        private readonly DaprClient _daprClient;
         private const string DAPR_STORE_NAME = "statestore";
 
-        public DaprVehicleStateRepository(HttpClient httpClient)
+        public DaprVehicleStateRepository(DaprClient daprClient)
         {
-            _httpClient = httpClient;
+            _daprClient = daprClient;
         }
         public async Task<VehicleState> GetVehicleStateAsync(string licenseNumber)
         {
-            var state = await _httpClient.GetFromJsonAsync<VehicleState>(
-                $"http://localhost:3600/v1.0/state/{DAPR_STORE_NAME}/{licenseNumber}");
-            return state;
+            return await _daprClient.GetStateAsync<VehicleState>(
+                DAPR_STORE_NAME, licenseNumber);
         }
 
         public async Task SaveVehicleStateAsync(VehicleState vehicleState)
         {
-            var state = new[]
-            {
-                new { 
-                    key = vehicleState.LicenseNumber,
-                    value = vehicleState
-                }
-            };
-
-            await _httpClient.PostAsJsonAsync(
-                $"http://localhost:3600/v1.0/state/{DAPR_STORE_NAME}",
-                state);
+            await _daprClient.SaveStateAsync(
+                DAPR_STORE_NAME, vehicleState.LicenseNumber, vehicleState);
         }
     }
 }
