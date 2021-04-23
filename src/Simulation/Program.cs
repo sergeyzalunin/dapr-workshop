d286d740fe34a8c39b0a6e9200786f3562292b20
Sergey Zalunin <Sergey.Zalunin@bki-okb.ru>
2021-04-23 08:34:11 +0300

    a6

--- src/Simulation/Program.cs
+++ src/Simulation/Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Net.Http;
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
 using Simulation.Proxies;
 
@@ -10,13 +8,12 @@ namespace Simulation
     {
         static void Main(string[] args)
         {
-            var httpClient = new HttpClient();
             int lanes = 3;
             CameraSimulation[] cameras = new CameraSimulation[lanes];
             for (var i = 0; i < lanes; i++)
             {
                 int camNumber = i + 1;
-                var trafficControlService = new HttpTrafficControlService(httpClient);
+                var trafficControlService = new MqttTrafficControlService(camNumber);
                 cameras[i] = new CameraSimulation(camNumber, trafficControlService);
             }
             Parallel.ForEach(cameras, cam => cam.Start());
